7fcabbb9884f5c17e2d8b54b07084286ad36462f
paulbg <paulbg@hey.com>
2024-01-29 10:58:45 +0200

    feat: implement root page

---
+++ frontend/src/routes/root.jsx
@@ -0,0 +1,12 @@
+import { Layout } from "../components/Layout";
+import { Header } from "../pagesComponents/index/Header";
+
+const Root = () => {
+  return (
+    <Layout>
+      <Header />
+    </Layout>
+  );
+};
+
+export default Root;
