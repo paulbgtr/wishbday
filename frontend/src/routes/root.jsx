c766bb3992542dd3383d7b67e22f2482667e4e43
paulbg <paulbg@hey.com>
2024-01-29 17:16:48 +0200

    feat: add Features and Ending to the root page

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,10 +1,14 @@
 import { Layout } from "../components/Layout";
 import { Header } from "../pagesComponents/index/Header";
+import { Features } from "../pagesComponents/index/Features";
+import { Ending } from "../pagesComponents/index/Ending";
 
 const Root = () => {
   return (
-    <Layout>
+    <Layout navbar className="justify-center py-32 gap-36 ">
       <Header />
+      <Features />
+      <Ending />
     </Layout>
   );
 };
