9ff6d637ca78874ebbf0f495621c87303bf6e45d
paulbg <paulbg@hey.com>
2024-01-29 17:16:22 +0200

    feat: new Ending component
    This component will be used to display the end of the page with a button to get started.

---
+++ frontend/src/pagesComponents/index/Ending.jsx
@@ -0,0 +1,11 @@
+export const Ending = () => {
+  return (
+    <div className="text-center">
+      <h2 className="mb-2 text-4xl font-bold">Keep Your Friends Happy</h2>
+      <p className="mb-4">Don&apos;t forget to wish them on their birthdays</p>
+      <a className="btn btn-primary" href="/dashboard">
+        Get wishbday
+      </a>
+    </div>
+  );
+};
