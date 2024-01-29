c267ed1789b763ac76d4d37618d6eb04232a10c8
paulbg <paulbg@hey.com>
2024-01-29 17:13:48 +0200

    feat: add footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="p-4 footer footer-center bg-base-300 text-base-content">
+      <aside>
+        <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+      </aside>
+    </footer>
+  );
+};
