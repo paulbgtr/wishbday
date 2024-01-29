8dfd9fc1ab3705f78dd7749f974e397cbc06585b
paulbg <paulbg@hey.com>
2024-01-29 10:58:04 +0200

    feat: implement dummy Header component

---
+++ frontend/src/pagesComponents/index/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <h2 className="mb-2 text-3xl font-bold">
+        Never miss a friend's birthday
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+        corrupti. Quam inventore unde, delectus hic, eligendi quod, cum sit
+        officia ullam cupiditate porro alias voluptate libero expedita quaerat
+        fuga ipsum.
+      </p>
+    </div>
+  );
+};
