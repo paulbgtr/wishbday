bdebc16b56b7b056257f8d44670956d46a9106bb
paulbg <paulbg@hey.com>
2024-01-30 10:04:39 +0200

    feat: add db file with requests to get and create a user

---
+++ backend/db/db.js
@@ -0,0 +1,24 @@
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/node-postgres";
+import pkg from "pg";
+const { Client } = pkg;
+import { users } from "./schemas.js";
+import "dotenv/config";
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+await client.connect();
+const db = drizzle(client);
+
+export const getUserByEmail = async (email) => {
+  return await db.select().from(users).where(eq(users.email, email));
+};
+
+export const createUser = async (email, password) => {
+  return await db
+    .insert(users)
+    .values({ email, password })
+    .returning({ insertedId: users.id });
+};
