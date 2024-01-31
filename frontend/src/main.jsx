678152880354d89f62d4c37c9e5032b7482150b6
paulbg <paulbg@hey.com>
2024-01-31 16:28:15 +0200

    feat: add routes for /sign-in and /sign-up

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
+import SignIn from "./routes/signin";
+import SignUp from "./routes/signup";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
   },
 ]);
 
