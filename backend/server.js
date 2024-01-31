b46ed29586f839aaba4b5104f334b1a5c3dd7696
paulbg <paulbg@hey.com>
2024-01-31 17:15:29 +0200

    feat: add cors to get requests from localhost:5173

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 import router from "./router.js";
 import bodyParser from "koa-bodyparser";
 import Koa from "koa";
+import cors from "@koa/cors";
 
 const app = new Koa();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(bodyParser());
 app.use(router.routes());
 
