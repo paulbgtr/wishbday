4de133f4691c55c50ce35956b882e3361836f58a
paulbg <paulbg@hey.com>
2024-02-02 10:45:40 +0200

    feat: implement Loading component

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+export const Loading = () => {
+  return (
+    <div className="min-h-screen hero bg-base-200">
+      <div className="text-center hero-content">
+        <div className="max-w-md">
+          <span className="loading loading-ball loading-lg"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
