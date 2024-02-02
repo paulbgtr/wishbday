1bec1bb4b00bbe809a7b797da794809b1f81e08d
paulbg <paulbg@hey.com>
2024-02-02 10:47:10 +0200

    feat: implement dashboard view when there is no contacts

---
+++ frontend/src/routes/dashboard.jsx
@@ -0,0 +1,38 @@
+import useAuth from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import { Loading } from "../components/Loading";
+import { Layout } from "../components/Layout";
+
+const Dashboard = () => {
+  const { isLoggedIn, isLoading } = useAuth();
+  const navigate = useNavigate();
+
+  if (!isLoggedIn) {
+    navigate("/sign-in");
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <Layout navbar className="min-h-screen">
+      <div className="hero">
+        <div className="text-center hero-content">
+          <div className="max-w-md">
+            <h2 className="text-3xl font-bold">
+              Echo, echo, echo... it&apos;s too quiet around here!
+            </h2>
+            <p className="py-6">
+              Kickstart the fun by adding some contacts. Let&apos;s transform
+              this ghost town into a lively gathering, shall we?
+            </p>
+            <button className="btn btn-primary">Bring on the Crowd</button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Dashboard;
