ab65bfe23c776aea3c92ab61cb899b05a9ac948e
paulbg <paulbg@hey.com>
2024-02-04 12:59:29 +0200

    feat: implement GET for contacts
    Implemented routes:
    
    - GET /users/:userId/contacts
    - GET /users/:userId/contacts/:contactId

--- backend/router.js
+++ backend/router.js
@@ -1,5 +1,6 @@
 import Router from "koa-router";
 import AuthController from "./controllers/auth.controller.js";
+import ContactsController from "./controllers/contacts.controller.js";
 
 const router = new Router();
 
@@ -11,5 +12,8 @@ router.post("/auth/sign-up", AuthController.signUp);
 router.post("/auth/sign-in", AuthController.signIn);
 router.get("/auth/sign-out", AuthController.signOut);
 router.get("/auth/me", AuthController.me);
+
+router.get("/users/:userId/contacts", ContactsController.getContacts);
+router.get("/users/:userId/contacts/:contactId", ContactsController.getContact);
 
 export default router;
