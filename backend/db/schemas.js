7086d80a319d6ba5af15fdb406c0f59e77eab0df
paulbg <paulbg@hey.com>
2024-02-04 12:35:00 +0200

    feat: implement schema for contacts table

--- backend/db/schemas.js
+++ backend/db/schemas.js
@@ -6,3 +6,17 @@ export const users = pgTable("users", {
   password: text("password").notNull(),
   createdAt: timestamp("created_at").notNull(),
 });
+
+export const contacts = pgTable("contacts", {
+  id: serial("id").primaryKey(),
+  userId: serial("user_id")
+    .notNull()
+    .references(() => users.id),
+  userEmail: serial("user_email").notNull(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+  wishTitle: text("wish_title").notNull(),
+  wishBody: text("wish_body").notNull(),
+  wishDate: timestamp("wish_date"),
+  createdAt: timestamp("created_at").notNull(),
+});
