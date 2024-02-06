5e2a755e4c392fe7141310b045becf6c2915de6e
paulbg <paulbg@hey.com>
2024-02-06 12:42:34 +0200

    feat: add logic for view display in dashboard
    If user has at least one contacts, they will be able to see the contacts list. If not, they will see a message to add a contact.
    The contact list is not yet implemented, so the message will be displayed for now.

--- frontend/src/routes/dashboard.jsx
+++ frontend/src/routes/dashboard.jsx
@@ -1,15 +1,39 @@
-import useAuth from "../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
-import { Loading } from "../components/Loading";
+import { useState, useEffect } from "react";
 import { Layout } from "../components/Layout";
+import useAuth from "../hooks/useAuth";
+import { Loading } from "../components/Loading";
 
 const Dashboard = () => {
-  const { isLoggedIn, isLoading } = useAuth();
-  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+  const { userProfile } = useAuth();
+  const [isDashboardEmpty, setIsDashboardEmpty] = useState(true);
 
-  if (!isLoggedIn) {
-    navigate("/sign-in");
-  }
+  useEffect(() => {
+    if (!userProfile.id) {
+      setIsLoading(false);
+      return;
+    }
+
+    const fetchContacts = async () => {
+      try {
+        const res = await fetch(
+          `http://localhost:3000/users/${userProfile.id}/contacts`
+        );
+        const resData = await res.json();
+
+        if (resData.contacts.length > 0) {
+          setIsDashboardEmpty(false);
+        }
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    setIsLoading(true);
+    fetchContacts();
+  }, [userProfile]);
 
   if (isLoading) {
     return <Loading />;
@@ -17,22 +41,26 @@ const Dashboard = () => {
 
   return (
     <Layout navbar>
-      <div className="hero">
-        <div className="text-center hero-content">
-          <div className="max-w-md">
-            <h2 className="text-3xl font-bold">
-              Echo, echo, echo... it&apos;s too quiet around here!
-            </h2>
-            <p className="py-6">
-              Kickstart the fun by adding some contacts. Let&apos;s transform
-              this ghost town into a lively gathering, shall we?
-            </p>
-            <a href="/contacts/add-contact" className="btn btn-primary">
-              Bring on the Crowd
-            </a>
+      {isDashboardEmpty ? (
+        <div className="hero">
+          <div className="text-center hero-content">
+            <div className="max-w-md">
+              <h2 className="text-3xl font-bold">
+                Echo, echo, echo... it&apos;s too quiet around here!
+              </h2>
+              <p className="py-6">
+                Kickstart the fun by adding some contacts. Let&apos;s transform
+                this ghost town into a lively gathering, shall we?
+              </p>
+              <a href="/contacts/add-contact" className="btn btn-primary">
+                Bring on the Crowd
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div>Hel</div>
+      )}
     </Layout>
   );
 };
